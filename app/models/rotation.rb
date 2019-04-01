e3f6b1065eb173cca14de846bfb7099bf55007b5
Tyler Chamberland <41761895+tylerchamberland@users.noreply.github.com>
2019-03-31 22:39:44 -0400

    add a topbar

--- app/models/rotation.rb
+++ app/models/rotation.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class Rotation < ApplicationRecord
-  validates :start, :end, :name, presence: true
+  validates :start, :end, presence: true
+  validates_presence_of :name, allow_blank: false
 end
